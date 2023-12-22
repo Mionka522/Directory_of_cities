8e4f012c7421f8309fc4185a137a802f85df14ec
Mionka522 <your.artist.alex@mail.ru>
2023-12-22 10:50:57 +0400

    Добавлена сортировка и меню для вывода на экран списков

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,13 +4,51 @@ import app.model.City;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        FileHandler fileHandler = new FileHandler();
-        ArrayList<City> parsedCities = fileHandler.parseCSV("Задача ВС Java Сбер.csv");
+    public static void main(String[] args) {
 
-        parsedCities.forEach(s -> System.out.println(s.toString()));
+        // FileHandler fileHandler = new FileHandler();
+        //ArrayList<City> parsedCities = fileHandler.parseCSV("Задача ВС Java Сбер.csv");
 
+        //parsedCities.forEach(s -> System.out.println(s.toString()));
+
+
+        menu();
     }
-}
+        public static void menu(){
+            Choice c;
+            c = new Choice();
+            int choice = c.getChoice();
+            FileHandler fileHandler = new FileHandler();
+            ArrayList<City> parsedCities = fileHandler.parseCSV("Задача ВС Java Сбер.csv");
+            switch (choice) {
+
+                case 1:
+                    parsedCities.forEach(s -> System.out.println(s.toString()));
+
+                    menu();//возврат к выбору действия
+
+                case 2:
+                    Collections.sort(parsedCities, (city1, city2) -> String.CASE_INSENSITIVE_ORDER.compare(city1.getName(), city2.getName()));
+                    parsedCities.forEach(s -> System.out.println(s.toString()));
+                    menu(); //возврат в меню
+                case 3:
+                    Collections.sort(parsedCities, Comparator.comparing(City::getDistrict).thenComparing(City::getName));
+                    parsedCities.forEach(s -> System.out.println(s.toString()));
+                    menu();//возврат в меню
+
+
+                case -1:
+                    System.out.println("Завершение программы");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Не могу распознать команду с номерм  " + choice+"\nПопытайтесь еще раз");
+                    menu();
+            }
+
+        }
+    }
